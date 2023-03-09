be62e10549b892ef63dca8ba17be60258df46561
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2023-03-09 13:38:51 +0100

    Set addon options

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,13 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["../preset.js", "@storybook/addon-essentials"],
+  addons: [
+    {
+      name: "../preset.js",
+      options: {
+        prefix: "template-react-component-library",
+      },
+    },
+    "@storybook/addon-essentials",
+  ],
 };
